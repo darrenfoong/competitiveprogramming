937f7a185d7a5bd00752cc498c900c74b6b7707f
Darren Foong <darrenfoong@gmail.com>
2016-12-01 23:45:05 +0800

    Add initial maxloot.cpp

---
+++ kattis/maxloot/maxloot.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+
+#define uint unsigned int
+#define pii pair<int, int>
+#define vi vector<int>
+#define vl vector<long>
+#define vpii vector<pair<int, int> >
+#define vvi vector<vector<int> >
+#define vvl vector<vector<long> >
+#define vvll vector<vector<long long> >
+#define vvpii vector<vector<pair<int, int> > >
+#define umapsi unordered_map<string, int>
+#define umapii unordered_map<int, int>
+#define umapllll unordered_map<long long, long long>
+#define mapii map<int, int>
+#define seti set<int>
+#define setpii set<pair<int, int> >
+#define mseti multiset<int>
+#define msetl multiset<long>
+#define msetlr multiset<long, greater<int> >
+#define pqi priority_queue<int>
+#define pqir priority_queue<int, vector<int>, greater<int> >
+#define pqpii priority_queue<pair<int, int> >
+#define pqpiir priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > >
+#define li list<int>
+#define forv(i,n) for ( int i = 0; i < n; i++ )
+#define forv1(i,n) for ( int i = 1; i <= n; i++ )
+#define all(x) x.begin(), x.end()
+#define pf printf
+#define sf scanf
+#define pb push_back
+#define nl "\n"
+
+#define DEBUG
+
+#ifdef DEBUG
+ #define db(x) x
+ #define io_opts
+#else
+ #define db(x)
+ #define io_opts ios_base::sync_with_stdio(false); cin.tie(NULL);
+#endif
+
+using namespace std;
+
+int solve(int n, int c, vi &values, vi &costs);
+
+int main() {
+    io_opts
+
+    int t = 0;
+    int n = 0;
+    int c = 0;
+    int value = 0;
+    int cost = 0;
+
+    cin >> t;
+    cin.ignore();
+
+    forv1(i,t){
+        cin >> n;
+        cin >> c;
+
+        vi values;
+        vi costs;
+
+        cin.ignore();
+
+        forv(j,n) {
+            cin >> value;
+            values.pb(value);
+        }
+
+        cin.ignore();
+
+        forv(j,n) {
+            cin >> cost;
+            costs.pb(cost);
+        }
+
+        cin.ignore();
+
+        cout << solve(n, c, values, costs) << nl;
+    }
+
+    return 0;
+}
+
+int solve(int n, int c, vi &values, vi &costs) {
+    int dp[n][c+1];
+    memset(dp, 0, sizeof(dp[0][0]) * n * (c+1));
+
+    for ( int j = 1; j < c+1; j++ ) {
+        if ( costs[0] < c ) {
+            dp[0][j] = values[0];
+        }
+    }
+
+    for ( int i = 1; i < n; i++ ) {
+        for ( int j = 0; j < c+1; j++ ) {
+            int pickValue = (j >= costs[i]) ? dp[i-1][j - costs[i]] + values[i] : 0;
+            int noPickValue = dp[i-1][j];
+            dp[i][j] = max(pickValue, noPickValue);
+        }
+    }
+
+    return dp[n-1][c];
+}
